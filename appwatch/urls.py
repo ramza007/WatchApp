ecd506d7ec10d0fc998355d9ff382f74ffe96b96
ramza007 <ramza007@gmail.com>
2018-05-26 11:34:02 +0300

    added search and drop down list

--- appwatch/urls.py
+++ appwatch/urls.py
@@ -1,12 +1,18 @@
 """
 App routes
 """
-#-----------------------------App Routes------------------------#
 
 
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+#-----------------------------App Routes------------------------#
 
 urlpatterns=[
     url(r'^$',views.index,name = 'index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
