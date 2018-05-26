9a10d5a9f874e234822ad6e76da626a84bde6178
ramza007 <ramza007@gmail.com>
2018-05-26 12:59:11 +0300

    added login and registration form

--- appwatch/urls.py
+++ appwatch/urls.py
@@ -3,7 +3,7 @@ App routes
 """
 
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -12,6 +12,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.index,name = 'index'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
 
 if settings.DEBUG:
