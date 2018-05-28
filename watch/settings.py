aa98fe9dc820c1ff2394794101ed2aa5d2ca5d33
ramza007 <ramza007@gmail.com>
2018-05-28 18:33:30 +0300

    heroku

--- watch/settings.py
+++ watch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ayi!byej+^kht3+kf43v3&3nx)mc7ktgy!cp@%rzqffgg&f^e)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
